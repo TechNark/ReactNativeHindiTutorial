8da76e0452532f6b76d1ca6ef531c5c1814f3d75
TechNark <121613761+TechNark@users.noreply.github.com>
2022-12-29 17:10:42 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import Counter from './src/containers/Counter/Counter';
+import List from './src/containers/List/List';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Counter);
